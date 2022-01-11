6bfc9f5d0ef0f9038edaad6732710572903f1e3a
murisf <murisf@hotmail.com>
2022-01-11 13:47:08 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var  poluprecnik = 6;
+var visina = 12;
+const PI = 3.14; 
+var  poluprecnik = prompt (`Unesite poluprečnik : 6`);
+var visina = prompt (`Unesite visinu : 12`);
+
+ 
+document.write(`Rezultat poluprecnika,visine i PI je : ${poluprecnik*visina*PI}`);
+
+
